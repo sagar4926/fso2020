80572e16fb875e4414ae686966c2c80c9e38ba93
Sagar B <sagar4926@gmail.com>
2020-04-10 16:58:15 +0530

    2.8: The Phonebook Step3

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number:"123" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addNewPerson = (event) => {
     event.preventDefault();
@@ -13,12 +14,17 @@ const App = () => {
       return;
     }
 
-    setPersons([...persons, { name: newName }]);
+    setPersons([...persons, { name: newName, number: newNumber }]);
     setNewName("");
+    setNewNumber("");
   };
 
   const nameChanged = (event) => {
     setNewName(event.target.value);
+  };
+
+  const numberChanged = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -29,13 +35,16 @@ const App = () => {
           name: <input value={newName} onChange={nameChanged} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={numberChanged} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} | {person.number}</li>
         ))}
       </ul>
     </div>
