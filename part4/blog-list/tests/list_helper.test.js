3df57165a172b9b43f6348ec53ba18ac5ad38415
Sagar B <sagar4926@gmail.com>
2020-04-16 15:00:19 +0530

    4.4: helper functions and unit tests, step2

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -5,3 +5,85 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe("like-counter", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5e9804870f8b8932436f5891",
+      title: "Test Blog",
+      author: "Author",
+      url: "https://google.com",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithManyBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url:
+        "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url:
+        "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0,
+    },
+  ];
+
+  test("when list has no blogs, it should return 0", () => {
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
+
+  test("when list has one blog, sum should be blog.likes", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test("when list has mang blogs, total blogs should be sum of likes of all blogs", () => {
+    const result = listHelper.totalLikes(listWithManyBlogs);
+    expect(result).toBe(36);
+  });
+});
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.map((blog) => blog.likes).reduce((acc, next) => acc + next, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
