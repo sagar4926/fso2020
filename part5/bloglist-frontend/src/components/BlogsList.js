17f820f58b41009b0c33d4289aa113a38fd10276
Sagar B <sagar4926@gmail.com>
2020-04-19 22:59:12 +0530

    5.9*: Blog list frontend, step8

--- part5/bloglist-frontend/src/components/BlogsList.js
+++ part5/bloglist-frontend/src/components/BlogsList.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Blog from "./Blog";
 
-const BlogsList = ({ blogs, onLike }) => (
-  <div>
-    <h2>blogs</h2>
-    {blogs.map((blog) => (
-      <Blog key={blog.id} blog={blog} onLike={onLike} />
-    ))}
-  </div>
-);
+const BlogsList = ({ blogs, onLike }) => {
+  blogs.sort((l, r) => r.likes - l.likes);
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} onLike={onLike} />
+      ))}
+    </div>
+  );
+};
 
 export default BlogsList;
