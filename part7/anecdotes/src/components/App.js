1838fd045e18cc272f71be2abeeeff72348183e0
Sagar B <sagar4926@gmail.com>
2020-04-30 21:08:51 +0530

    7.1: routed anecdotes, step1

---
+++ part7/anecdotes/src/components/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import NavBar from "./NavBar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Anecdotes from "./Anecdotes";
+import About from "./About";
+import CreateAnecdoteForm from "./CreateAnecdoteForm";
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: "If it hurts, do it more often",
+      author: "Jez Humble",
+      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
+      votes: 0,
+      id: "1",
+    },
+    {
+      content: "Premature optimization is the root of all evil",
+      author: "Donald Knuth",
+      info: "http://wiki.c2.com/?PrematureOptimization",
+      votes: 0,
+      id: "2",
+    },
+  ]);
+
+  const [notification, setNotification] = useState("");
+
+  const addNew = (anecdote) => {
+    anecdote.id = (Math.random() * 10000).toFixed(0);
+    setAnecdotes(anecdotes.concat(anecdote));
+  };
+
+  const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id);
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    };
+
+    setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
+  };
+
+  return (
+    <div>
+      <Header />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/anecdotes">
+            <Anecdotes anecdotes={anecdotes} />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/create">
+            <CreateAnecdoteForm addNew={addNew} />
+          </Route>
+          <Route path="/">
+            <Redirect to="/anecdotes"></Redirect>
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ part7/anecdotes/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h1>Software anecdotes</h1>;
+};
+
+export default Header;
