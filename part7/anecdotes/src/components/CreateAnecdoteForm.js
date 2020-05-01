8f9d3ae223eb13b4b6c713a71b5aae0ee893cd8e
Sagar B <sagar4926@gmail.com>
2020-05-01 08:32:43 +0530

    7.4: anecdotes and hooks step1

--- part7/anecdotes/src/components/CreateAnecdoteForm.js
+++ part7/anecdotes/src/components/CreateAnecdoteForm.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import useField from "../hooks/UseField";
 
 const CreateAnecdoteForm = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("content");
+  const author = useField("author");
+  const info = useField("info");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
   };
@@ -21,27 +22,15 @@ const CreateAnecdoteForm = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
---
+++ part7/anecdotes/src/hooks/UseField.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const useField = (name) => {
+  const [value, setValue] = useState("");
+  const onChange = (event) => setValue(event.target.value);
+  return {
+    name,
+    value,
+    onChange,
+  };
+};
+
+export default useField;
