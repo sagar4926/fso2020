79eae766078d584952a763aa57004547191e35bc
Sagar B <sagar4926@gmail.com>
2020-05-02 15:20:08 +0530

    Moved private paths to dashboard component. Navbar is no longer visible on login page.

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,60 +1,28 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch, Redirect } from "react-router-dom";
-import AddBlogForm from "./components/AddBlogForm/AddBlogForm";
-import Blog from "./components/Blog/Blog";
-import BlogsList from "./components/BlogsList";
+import { Route, Switch } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
-import NavBar from "./components/NavBar";
 import Notifications from "./components/Notifications/Notifications";
-import Togglable from "./components/Togglable";
-import User from "./components/User";
-import Users from "./components/Users";
-import { initBlogs } from "./redux/reducers/blogsReducer";
-import { initUser } from "./redux/reducers/userReducer";
-import { initUsers } from "./redux/reducers/usersReducer";
 import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./Dashboard";
+import { initUser } from "./redux/reducers/userReducer";
 
 const App = () => {
-  // Using this over React.createRef persists the ref across state changes and re-renders
-  const blogFormRef = useRef(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(initUser());
-    dispatch(initBlogs());
-    dispatch(initUsers());
   }, []);
-
-  const blogAdded = () => {
-    blogFormRef.current.toggleVisiblity();
-  };
 
   return (
     <>
       <Notifications />
-      <NavBar />
       <Switch>
         <Route path="/login">
           <LoginForm />
         </Route>
-        <PrivateRoute path="/users/:id">
-          <User />
-        </PrivateRoute>
-        <PrivateRoute path="/users">
-          <Users />
-        </PrivateRoute>
-        <PrivateRoute path="/blogs/:id">
-          <Blog />
-        </PrivateRoute>
-        <PrivateRoute path="/blogs">
-          <Togglable buttonText="Add Blog" ref={blogFormRef}>
-            <AddBlogForm onBlogAdded={blogAdded}></AddBlogForm>
-          </Togglable>
-          <BlogsList />
-        </PrivateRoute>
         <PrivateRoute path="/">
-          <Redirect to="/blogs" />
+          <Dashboard />
         </PrivateRoute>
       </Switch>
     </>
---
+++ part7/bloglist-frontend/src/Dashboard.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { Redirect, Route, Switch } from "react-router-dom";
+import AddBlogForm from "./components/AddBlogForm/AddBlogForm";
+import Blog from "./components/Blog/Blog";
+import BlogsList from "./components/BlogsList";
+import NavBar from "./components/NavBar";
+import PrivateRoute from "./components/PrivateRoute";
+import Togglable from "./components/Togglable";
+import User from "./components/User";
+import Users from "./components/Users";
+import { initBlogs } from "./redux/reducers/blogsReducer";
+import { initUsers } from "./redux/reducers/usersReducer";
+
+const Dashboard = () => {
+  // Using this over React.createRef persists the ref across state changes and re-renders
+  const blogFormRef = useRef(null);
+  const blogAdded = () => {
+    blogFormRef.current.toggleVisiblity();
+  };
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initBlogs());
+    dispatch(initUsers());
+  }, []);
+
+  console.log("Dashboard render");
+
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route path="/users/:id">
+          <User />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/blogs/:id">
+          <Blog />
+        </Route>
+        <Route path="/blogs">
+          <Togglable buttonText="Add Blog" ref={blogFormRef}>
+            <AddBlogForm onBlogAdded={blogAdded}></AddBlogForm>
+          </Togglable>
+          <BlogsList />
+        </Route>
+        <PrivateRoute path="/">
+          <Redirect to="/blogs" />
+        </PrivateRoute>
+      </Switch>
+    </>
+  );
+};
+export default Dashboard;
