be88bfc2eb138bbc1213409874d9d26ea39d7a64
Sagar B <sagar4926@gmail.com>
2020-05-03 20:23:47 +0530

    8.7: Updating the birth year of an author

--- part8/books-and-authors/index.js
+++ part8/books-and-authors/index.js
@@ -109,6 +109,7 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -145,6 +146,15 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) {
+        return author;
+      }
+      const authorToUpdate = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.id === author.id ? authorToUpdate : a));
+      return authorToUpdate;
     },
   },
 };
