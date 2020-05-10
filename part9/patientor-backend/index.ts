5085f4cc0adf6f2ffa2155decb031110d2820ced
Sagar B <sagar4926@gmail.com>
2020-05-10 18:32:21 +0530

    9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-const PORT = 3000;
+const PORT = 3001;
 
 app.get("/", (_req, res) => {
   res.send("");
 });
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
